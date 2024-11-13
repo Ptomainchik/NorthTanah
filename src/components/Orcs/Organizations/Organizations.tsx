4c67cdda225b68526e86770845b0dd48ab35a2ed
Ptomainchik <fantasticdenis@mail.ru>
2024-11-13 22:45:39 +0300

    Image

--- src/components/Orcs/Organizations/Organizations.tsx
+++ src/components/Orcs/Organizations/Organizations.tsx
@@ -10,44 +10,53 @@ export const Organizations = () => {
     return <div className={classes.organizations}>
       <h2>Организации</h2>
  <div className={classes.organizationsLists}>
- <ol className={classes.elemOrganizationList}>
-  <p><strong>Ловцы духов:</strong></p>
-  <strong>
-     <li>Норгуз</li>
-</strong>
- </ol>
- 
- <ol className={classes.elemOrganizationList2}>
-  <p><strong>Охотники:</strong></p>
-  <strong>
-    <li>Малгар</li>
-    <li>Бурзаг</li>
-  </strong>
- </ol>
-
- <ol className={classes.elemOrganizationList3}>
-  <p><strong>Работники:</strong></p>
-  <strong>
-    <li>Вакшиг</li>
-  </strong>
- </ol>
-
- <ol className={classes.elemOrganizationList4}>
-  <p><strong>Банды:</strong></p>
-   <strong>
-    <li>Родзарик</li>
-    <li>Фругеш</li>
-    <li>Хармук</li>
-   </strong>
- </ol>
+    <div className={classes.organizationBlock}>
+        <ol className={classes.elemOrganizationList}>
+          <p><strong>Ловцы духов:</strong></p>
+          <strong>
+          <li>Норгуз</li>
+          </strong>
+        </ol>
+        <SpiritCatchers/>
+    </div>
+    <div className={classes.organizationBlock}>
+        <ol className={classes.elemOrganizationList2}>
+          <p><strong>Охотники:</strong></p>
+          <strong>
+          <li>Малгар</li>
+          <li>Бурзаг</li>
+          </strong>
+        </ol>
+        <Hunters/>
+    </div>   
  </div>
- <div className={classes.organizationsIconsButton}>
- <Workers/>
- <Hunters/>
- <Gangs/>
- <SpiritCatchers/>
+
+ <div className={classes.organizationsLists}>
+    <div className={classes.organizationBlock}>
+        <ol className={classes.elemOrganizationList}>
+          <p><strong>Работники:</strong></p>
+          <strong>
+          <li>Вакшиг</li>
+          </strong>
+        </ol>
+        <Workers/>
+    </div>
+    <div className={classes.organizationBlock}>
+        <ol className={classes.elemOrganizationList3}>
+          <p><strong>Банды:</strong></p>
+          <strong>
+          <li>Родзарик</li>
+          <li>Фругеш</li>
+          <li>Хармук</li>
+          </strong>
+        </ol>
+        <Gangs/>
+    </div>
+    </div>
+ <div>
+  <TableOrganizations/>
  </div>
- <TableOrganizations/>
-</div>
+ </div>
+
 
 }

eda25bd52921b5735a662f969a986109338f37b2
Ptomainchik <fantasticdenis@mail.ru>
2024-11-15 02:32:22 +0300

    Image2

--- src/components/Orcs/HistoryOrcs/HistoryOrcs.tsx
+++ src/components/Orcs/HistoryOrcs/HistoryOrcs.tsx
@@ -1,14 +1,14 @@
 import classes from './../Styles/OrcsKingdom.module.css'
-import BannerOfBearPeaks from '../../../images/Dwarves/HistoryBearPeaks/BannerOfBearPeaks.webp'
+import BannerOfOrcs from '../../../images/Orcs/HistoryOrcsForest/BannerOfOrcs.webp'
 import { AccordionState } from './AccordionOrcsKingdom/AccordionState'
 
 export const HistoryOfOrcsKingdom = () => {
     return (
   <div className={classes.bodyHistory}>
   <div className={classes.titleHistory}>
-  <img title="Знамя "   alt="Герб" src={BannerOfBearPeaks} className={classes.Banner}/>
+  <img title="Знамя "   alt="Герб" src={BannerOfOrcs} className={classes.banner}/>
   <h1>Orcs Kingdom</h1>
-  <img title="Знамя"  alt="Герб" src={BannerOfBearPeaks} className={classes.Banner}/>
+  <img title="Знамя"  alt="Герб" src={BannerOfOrcs} className={classes.banner}/>
  </div>
  <AccordionState/>
   </div>

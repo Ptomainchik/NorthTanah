524b7616e335604283785df19cbcb684f0feac10
Ptomainchik <fantasticdenis@mail.ru>
2024-12-01 22:48:12 +0300

    StartPage

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -8,6 +8,11 @@ export const HomePage = () => {
     <div className={classes.choiceTitle}>Tanah <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, commodi quidem. Consequuntur odit aut nihil iusto, vel tenetur. 
         Tempora perferendis id, quae repellendus veniam molestias fugit error doloremque voluptas! Qui.</p>
     </div>
+    <div className={classes.triangles}>
+        <div className={classes.blueTriangle}></div>
+        <div className={classes.greenTriangle}></div>
+    </div>
+       
     <DwarvesSelectionButton/>
     <OrcsSelectionButton/>
 </div>

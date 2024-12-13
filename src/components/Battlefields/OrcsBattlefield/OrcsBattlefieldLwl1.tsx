3ffee60515ed9f00832797cdf825169b4d6e8e5a
Ptomainchik <fantasticdenis@mail.ru>
2024-12-13 04:09:00 +0300

    Battlefield5

--- src/components/Battlefields/OrcsBattlefield/OrcsBattlefieldLwl1.tsx
+++ src/components/Battlefields/OrcsBattlefield/OrcsBattlefieldLwl1.tsx
@@ -1,31 +1,50 @@
+import { useState } from "react";
 import { BackButton } from "../../Button/BackButton/BackButton";
 import { HomeButton } from "../../Button/HomeButton/HomeButton";
 import classes from "../../Styles/BattlefieldsStyles.module.css";
-import { DwarvesUnitLwl1 } from "../Units/DwarvesUnits/DwarvesUnitLwl1";
-import { OrcsUnitLwl1 } from "../Units/OrcsUnits/OrcsUnitLwl1";
 
+
+const randomIntegerFromInterval = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 export const OrcsBattlefieldLwl1 = () => {
+    const [dwarfHealth1, setDwarfHealth1] = useState(1000);
+    const [orcHealth1, setOrcHealth1] = useState(1000);
+    const [diceRoll, setDiceRoll] = useState(0);
+
+    const rollDice = () => {
+        setDiceRoll(randomIntegerFromInterval(1, 2));
+    };
+
+    const handleOrcAttack = () => {
+        rollDice();
+        if (dwarfHealth1 > 0 && diceRoll !== null) {
+            setDwarfHealth1(dwarfHealth1 - diceRoll * 10);
+            setTimeout(() => {
+                setOrcHealth1(orcHealth1 - 10);
+            }, 100);
+        }
+    };
+
     return (
-    <div className={classes.orcsBattlefield}>
-        <div className={classes.buttonOrcsBattlefield}><HomeButton/><span>Battlefield</span><BackButton/></div>
-        <div className={classes.unitsFlex}>
-            <div className={classes.unitsBlock}>
-                <div className={classes.dwarfLwl12}>
-                    <DwarvesUnitLwl1/>
-                    <DwarvesUnitLwl1/>
-                    <DwarvesUnitLwl1/>
-                    <DwarvesUnitLwl1/>
+        <div className={classes.orcsBattlefield}>
+            <div className={classes.buttonOrcsBattlefield}><HomeButton/><span>Battlefield</span><BackButton/></div>
+            <div className={classes.unitsFlex}>
+                <div className={classes.unitsBlock}>
+                    <div className={classes.dwarfLwl12}>
+                        {dwarfHealth1}<button className={classes.buttonDwarfLwl1} onClick={handleOrcAttack} ></button>
+                    </div>
                 </div>
-            </div>
-            <div className={classes.unitsBlock}>
-                <div className={classes.orcLwl12}>
-                    <OrcsUnitLwl1/>
-                    <OrcsUnitLwl1/>
-                    <OrcsUnitLwl1/>
-                    <OrcsUnitLwl1/>
+                <div>{diceRoll}</div>
+                <div className={classes.unitsBlock}>
+                    <div className={classes.orcLwl12}>
+                        {orcHealth1}<button className={classes.buttonOrcLwl1}></button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>)
-}
+    );
+};
+
+export default OrcsBattlefieldLwl1;

0fc4c9bdbe1fb659ef92503e4588fa4131f97abc
Ptomainchik <fantasticdenis@mail.ru>
2025-01-23 03:28:24 +0300

    RefactoringCss1.1

--- src/components/Orcs/OrcsPage/OrcsPage.tsx
+++ src/components/Orcs/OrcsPage/OrcsPage.tsx
@@ -7,23 +7,22 @@ import { HomeButton } from '../../Button/HomeButton/HomeButton'
 export const OrcsPage = () => {
     return <div>
       <div className={classes.button}><HomeButton/><span>Orcs</span><BackButton/></div>
-    <div className={classes.info}> 
+<div className={classes.info}> 
   <div className={classes.blockPage}>
-     <div className={classes.infoPage}>
+    <div className={classes.infoPage}>
            <img title="Орк" src={OrcShaman} alt="Орк" className={classes.imgPage}/>
          <div className={classes.infoTextPage}>
-           <h2>Орки</h2>
-           <h6>Чёрный лес</h6>
+           <h6>Орки</h6>
+           <p>Чёрный лес</p>
         </div>
-    </div>
-    <div>
+  </div>
+    <div className={classes.blockLink}>
           <Link to="/historybf" title="История Чёрного Леса" className={classes.orcsPageButton} ><h5>The history of Black Forest</h5></Link>
           <Link to="/communities" title="Общины Чёрного Леса" className={classes.orcsPageButton} ><h5>Communities of Black Forest</h5></Link>
           <Link to="/battlefieldorlvl1" title="Боевое поле" className={classes.orcsPageButton} ><h5>Battlefield</h5></Link>
     </div>
   </div>   
   <div className={classes.story}>
-    <div className={classes.storyText}>
       <p>
         Раса орков представляет из себя - зеленокожих существ среднего роста, крепкого телосложения. Они отнюдь не глупые, а даже очень смекалистые.
         Ведь только у них получилось выжить и создать королевство в дремучем, мрачном Чёрном лесу.
@@ -32,7 +31,7 @@ export const OrcsPage = () => {
         Сама природа не подразумевает ни какой души у них. Дух матери поддерживает жизнь плода в утробе, в момент отделения пуповины младенец умирает. Но оживает вновь, после дарования ему духа предка от хранителя.
         Лишь несколько раз в году, рождаются орки с собственным духом, случается это только если духов родителей так же влечёт друг к другу!
       </p>
-    </div>
   </div>
-</div></div>
+</div>
+</div>
 }

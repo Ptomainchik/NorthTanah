2b2f3dc3585c722d52dd64981f2ff5610202f64f
Ptomainchik <fantasticdenis@mail.ru>
2025-02-07 21:55:24 +0300

    Ref1.7

--- src/components/Button/RestartLevelButton/RestartLevelButton.tsx
+++ src/components/Button/RestartLevelButton/RestartLevelButton.tsx
@@ -1,12 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 import classes from '../../Styles/Styles.module.css'
 
-export function RestartButton() {
+export function RestartLevelButton() {
   const navigate = useNavigate();
   const handleClick = () => {
     const currentPath = window.location.pathname;
     navigate(currentPath);
-  }
+  };
 
   return (
     <div className={classes.button}>

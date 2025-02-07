326ae12c567d1ff9b85ea8e17488b4cdd62de663
Ptomainchik <fantasticdenis@mail.ru>
2025-02-07 20:20:59 +0300

    reload

--- src/components/Button/RestartLevelButton/RestartLevelButton.tsx
+++ src/components/Button/RestartLevelButton/RestartLevelButton.tsx
@@ -1,10 +1,8 @@
-import { useNavigate } from 'react-router-dom';
 import classes from '../../Styles/Styles.module.css'
 
 export function RestartLevelButton() {
-  let navigate = useNavigate();
   function handleClick() {
-    navigate(0);
+    window.location.reload();
   }
 
   return (
